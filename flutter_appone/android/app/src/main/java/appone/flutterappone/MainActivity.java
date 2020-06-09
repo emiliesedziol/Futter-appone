b34f0904a8805193c0d984b2f93e7ac48850a652
emiliesedziol <sedziole@gmail.com>
2020-06-09 11:37:19 -0400

    add appone

---
+++ flutter_appone/android/app/src/main/java/appone/flutterappone/MainActivity.java
@@ -0,0 +1,6 @@
+package appone.flutterappone;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ flutter_appone/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('app one'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],  // ctrl-q doesn't bring up a list of colors
+
+      ),
+      body: Center(
+        child: Text(
+          'In the body',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[800],
+            fontFamily: 'IndieFlower',
+
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},  // onPressed is required for a button action
+        child: Text('Click this button'),
+        backgroundColor: Colors.red[800],
+      ),
+      // this has changed
+
+    ),
+
+
+  ));
