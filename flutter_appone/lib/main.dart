f396ce4a67f088cae12c2ffdac49d5a68b10e86e
emiliesedziol <sedziole@gmail.com>
2020-06-09 12:13:47 -0400

    stateless reload

--- flutter_appone/lib/main.dart
+++ flutter_appone/lib/main.dart
@@ -1,36 +1,48 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
+void main() => runApp(MaterialApp(
 
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('app one'),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],  // ctrl-q doesn't bring up a list of colors
+    home: Home()
+  ));
+// statless widget is needed to refresh the virtual device
 
-      ),
-      body: Center(
-        child: Text(
-          'In the body',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[800],
-            fontFamily: 'IndieFlower',
+// to get this 'stless' - need to enter all the characters of it will be wrong
 
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},  // onPressed is required for a button action
-        child: Text('Click this button'),
-        backgroundColor: Colors.red[800],
-      ),
-      // this has changed
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+    title: Text('app one'),
+    centerTitle: true,
+    backgroundColor: Colors.red[600],  // ctrl-q doesn't bring up a list of colors
 
     ),
+    body: Center(
+    child: Text(
+    'In the body',
+    style: TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 2.0,
+    color: Colors.grey[800],
+    fontFamily: 'IndieFlower',
+
+    ),
+    ),
+    ),
+    floatingActionButton: FloatingActionButton(
+    onPressed: () {},  // onPressed is required for a button action
+  child: Text('Click!'),
+  backgroundColor: Colors.red[800],
+  ),
+  // this has changed
+
+  )
+
+  ;
+  }
+}
 
 
-  ));
